ee3a05097eff662f2d9170bf5f77f4b7cfa78655
Luiz <lcmscontas@gmail.com>
2022-01-16 11:51:49 -0300

    Save score;

--- backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovie/entities/Movie.java
@@ -1,16 +1,19 @@
 package com.devsuperior.dsmovie.entities;
 
 import java.io.Serializable;
+import java.util.HashSet;
+import java.util.Set;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_movie")
-public class Movie implements Serializable{
+public class Movie implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
@@ -20,7 +23,10 @@ public class Movie implements Serializable{
 	private Double score;
 	private Integer count;
 	private String image;
-	
+
+	@OneToMany(mappedBy = "id.movie")
+	private Set<Score> scores = new HashSet<>();
+
 	public Movie() {
 	}
 
@@ -71,6 +77,9 @@ public class Movie implements Serializable{
 	public void setImage(String image) {
 		this.image = image;
 	}
-	
-	
+
+	public Set<Score> getScores() {
+		return scores;
+	}
+
 }
